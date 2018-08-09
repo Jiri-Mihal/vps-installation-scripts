eb485b978be1cdcc9d032ee67ee8dae9a126c57d
Jiri Mihal <jiri@mihal.me>
2018-08-09 20:29:41 +0200

    Removed unnecessary line.

--- backup-to-ftp.sh
+++ backup-to-ftp.sh
@@ -8,7 +8,6 @@ RED="\033[1;31m"
 NC="\033[0m"
 
 # Gather required parameters from user
-echo -e "Enter the domain eq ${RED}yourdomain.tld${NC} associated with this app:"
 read DOMAIN
 if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
 	echo -e "Aborted. Missing required parameters. Call should be: bash backup-to-ftp.sh ${RED}yourftp.tld user pswd 'sources' destination${NC}"
